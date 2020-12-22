174bdff1d5ff0090b6587a408d28888f0d2c9130
shazy89 <erdo.shazy123@gmail.com>
2020-12-22 11:24:11 -0500

    connected to 3 party api and display data

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,9 @@
-import axios from 'axios'
-//import {API_KEY, CLIENT_ID} from "@env"
+import axios from 'axios';
+
 export default axios.create({
-    baseURL: 'GET https://api.yelp.com/v3/businesses',
-    headers: {
-        Authorization: `Bearer ejCu1DPTSYmxbVrBxCDYkkscyQ-Qp2j7my_eUb6RK6h7XkVyecvaLqUSIw54TlwYoKAuuMVs2suEqa4v4KyNgcG0Of0iGZRy11HYyKoK-l-86wVk9sQ-mk8-1OrgX3Yx`
-    } 
-})
+  baseURL: 'https://api.yelp.com/v3/businesses',
+  headers: {
+    Authorization:
+      'Bearer ejCu1DPTSYmxbVrBxCDYkkscyQ-Qp2j7my_eUb6RK6h7XkVyecvaLqUSIw54TlwYoKAuuMVs2suEqa4v4KyNgcG0Of0iGZRy11HYyKoK-l-86wVk9sQ-mk8-1OrgX3Yx'
+  }
+});
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,30 +1,43 @@
-import React, {useState} from 'react';
-import { Text, StyleSheet, View, Image, TextInput, ImageBackground } from "react-native";
+import React, {useState, useEffect} from 'react';
+import { Text, StyleSheet, View, Image, TextInput, ImageBackground, Button  } from "react-native";
 import SearchBar from '../components/SearchBar'
 import yelp from '../api/yelp'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('')
     const [results, setResults] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
 
     const searchApi = async () => {
+    //    /search?limit=50 query string
+        try {
       const response = await yelp.get('/search', {
           params: {
-              
+            limit:50,
+            term,
+            location: 'san jose'  
           }
       })
+      console.log(response)
       setResults(response.data.businesses)
-       
-    }
-         
-   return (
-       <View> 
-           <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} onTearmSubmit={() => console.log('term was submitted')}/>
-           <Text>{term}</Text>
-           <Text>LENGTH IS - {results.length}</Text>
-       </View>
-   );
+    } catch (err) {
+        setErrorMessage('Something went wrong')
+    } 
+ }
+
+useEffect(() => searchApi(),[])
+ return (
+     <View> 
+         <SearchBar term={term} 
+                    onTermChange={setTerm} 
+                    onTearmSubmit={searchApi}/>
+          { errorMessage ? <Text>{errorMessage}</Text> : null}
+         <Text>LENGTH IS - {results.length}</Text>
+
+     </View>
+ );
 };
+         
          
            
 
