b3b79cf87007d4179c4db9c4bf02e26ccc3d74c7
shazy89 <erdo.shazy123@gmail.com>
2020-12-22 13:37:27 -0500

    display images, restaurant raitings and review count

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Text, StyleSheet, View, Image, TextInput, ImageBackground } from "react-native";
+import { Text, StyleSheet, View, Image } from "react-native";
 
 
 const ResultDetail = ({result}) => {
@@ -7,7 +7,9 @@ console.log(result)
 
    return (
        <View> 
+             <Image style={styles.img} source={{uri: result.image_url}} />
             <Text>{result.name}</Text>
+            <Text>{result.reting} Stars, {result.review_count} Reviews</Text>
        </View>
    );
 };
@@ -15,7 +17,15 @@ console.log(result)
       
 
 const styles = StyleSheet.create({
-
+    img: {
+        width: 250,
+        borderRadius: 4,
+        height: 120
+      },
+      name: {
+          fontWeight: 'bold',
+          
+      }
 })
 
 export default ResultDetail
