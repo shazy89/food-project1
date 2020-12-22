c5bd4ea62ff1e34f9588d4497e311609d130a6be
shazy89 <erdo.shazy123@gmail.com>
2020-12-22 09:43:45 -0500

    src api

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'GET https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: `Bearer `
+        Authorization: `Bearer ejCu1DPTSYmxbVrBxCDYkkscyQ-Qp2j7my_eUb6RK6h7XkVyecvaLqUSIw54TlwYoKAuuMVs2suEqa4v4KyNgcG0Of0iGZRy11HYyKoK-l-86wVk9sQ-mk8-1OrgX3Yx`
     } 
 })
