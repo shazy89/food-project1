79a03e4e2df4190418f5128d0087d781043ce014
shazy89 <erdo.shazy123@gmail.com>
2020-12-22 16:05:46 -0500

    remove yelp

--- src/api/yelp.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/businesses',
-  headers: {
-    Authorization:
-      'Bearer ejCu1DPTSYmxbVrBxCDYkkscyQ-Qp2j7my_eUb6RK6h7XkVyecvaLqUSIw54TlwYoKAuuMVs2suEqa4v4KyNgcG0Of0iGZRy11HYyKoK-l-86wVk9sQ-mk8-1OrgX3Yx'
-  }
-});
